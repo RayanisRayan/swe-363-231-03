49ba95f27ba0e9d4b263993cb32d44fdd1426aa0
RayanisRayan <Rayanalipersonal@gmail.com>
2023-11-08 01:46:51 +0300

    BE2_EX2 Update

--- BE/BE2.js
+++ BE/BE2.js
@@ -1,36 +1,3 @@
-// const fs = require('fs');
-// const path = require('path');
-// const { promisify } = require('util');
-
-// const readdir = promisify(fs.readdir);
-// const copyFile = promisify(fs.copyFile);
-
-// const directSrc = process.argv[2];
-// const directTar = process.argv[3];
-
-// async function redir(directSrc, directTar, extensions) {
-//   try {
-//     const files = await readdir(directSrc);
-
-//     for (const file of files) {
-//       const FE = path.extname(file).toLowerCase();
-
-//       if (extensions.includes(FE)) {
-//         const sourcep = path.join(directSrc, file);
-//         const tp = path.join(directTar, file);
-
-//         await copyFile(sourcep, tp);
-//         console.log(`${file} is copied.`);
-//       }
-//     }
-//   } catch (error) {
-//     console.error('Error while copying', error);
-//   }
-// }
-
-// const allowedExtensions = ['.txt', '.jpg'];
-
-// redir(directSrc, directTar, allowedExtensions);
 const fs = require('fs');
 const path = require('path');
 const { promisify } = require('util');
@@ -41,27 +8,26 @@ const copyFile = promisify(fs.copyFile);
 const directSrc = process.argv[2];
 const directTar = process.argv[3];
 
-async function copy(directSrc, directTar, extensions) {
+async function redir(directSrc, directTar, extensions) {
   try {
-    console.log(directSrc)
-    const files = await readdir(directSrc+"");
+    const files = await readdir(directSrc);
 
     for (const file of files) {
-      const fileExtension = path.extname(file).toLowerCase();
+      const FE = path.extname(file).toLowerCase();
 
-      if (extensions.includes(fileExtension)) {
-        const pathSrc = path.join(directSrc, file);
-        const pathTar = path.join(directTar, file);
+      if (extensions.includes(FE)) {
+        const sourcep = path.join(directSrc, file);
+        const tp = path.join(directTar, file);
 
-        await copyFile(pathSrc, pathTar);
+        await copyFile(sourcep, tp);
         console.log(`${file} is copied.`);
       }
     }
-  } catch (err) {
-    console.error('Error while copying', err);
+  } catch (error) {
+    console.log(error);
   }
 }
 
-const allowedExtensions = ['.txt', '.jpg'];
+const Extention = ['.txt', '.jpg'];
 
-copy(directSrc, directTar, allowedExtensions);
+redir(directSrc, directTar, Extention);
